cc7acbf8cf5543621a6c8e9c723f59ef35b6aeca
ontime consultants <ontime.c23@gmail.com>
2024-05-05 02:04:10 +0300

    Changes to be committed: 	modified:   app/(root)/layout.tsx 	modified:   app/(root)/page.tsx 	deleted:    app/page.tsx 	new file:   components/AnimatedCountUp.tsx 	new file:   components/DoughnutChart.tsx 	new file:   components/HeaderBox.tsx 	new file:   components/MobileNavBar.tsx 	new file:   components/Sidebar.tsx 	new file:   components/TotalBalanceBox.tsx 	new file:   components/ui/sheet.tsx 	modified:   lib/utils.ts 	modified:   package-lock.json 	modified:   package.json 	modified:   types/index.d.ts

---
+++ components/Sidebar.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+const Sidebar = ({ user }: SiderbarProps) => {
+
+    const pathName = usePathname();
+
+  return (
+    <section className='sidebar'>
+        <nav className='flex flex-col gap-4'>
+            <Link href={'/'}
+                className='flex mb-12 cursor-pointer items-center gap-2'
+            >
+                <Image src='/icons/logo.svg' alt='logo' width={34} height={34} className='size-[24px] max-xl:size-14'  />
+                <h1 className='sidebar-logo'>Martin's</h1>
+            </Link>
+            { sidebarLinks.map((link, index) => {
+                const isActive = pathName === link.route || pathName.startsWith(`${link.route}/`)
+                return (
+                    <Link href={link.route} key={index} className={cn('sidebar-link', {'bg-bank-gradient': isActive })}>
+                        <div className='relative size-6'>
+                            <Image src={link.imgURL} alt={link.label} fill  className={ cn({ 'brightness-[3] invert-0': isActive })} />
+                        </div>
+                        <p className={cn('sidebar-label',{'!text-white': isActive })}>{link.label}</p>   
+                    </Link>
+                )
+            } )}
+
+            USER
+        </nav>
+
+        FOOTER
+    </section>
+  )
+}
+
+export default Sidebar
