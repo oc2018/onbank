211d756e1aa938068d197f4fe732fd57c037c3bf
Eric Ndege <ontime.c23@gmail.com>
2024-08-04 07:31:02 +0300

    little typo

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,7 +9,7 @@ import React from 'react'
 const Home = async({ searchParams: { id, page }}: SearchParamProps) => {
   const currentPage = Number(page as string) || 1;
   const loggedIn = await getLoggedInUser();
-  const accounts = await getAccounts({ userId: loggedIn.$id })
+  const accounts = await getAccounts({ userId: loggedIn?.$id })
 
   if(!accounts) return;
 
